6786beb3bd7e444334ffb9bcd8150e0d72bbf00b
Chaidar Saad <chaidarsaad55@gmail.com>
2024-02-14 12:23:22 +0700

    update categories

--- app/Filament/Resources/CategoryResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/CategoryResource/Widgets/StatsOverview.php
@@ -18,16 +18,7 @@ class StatsOverview extends BaseWidget
         return [
             Stat::make('Total Category', Category::count())
                 ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->color('success'),
-            Stat::make('Unique views', '192.1k')
-                ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up'),
-            Stat::make('Bounce rate', '21%')
-                ->description('7% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-down'),
-            Stat::make('Average time on page', '3:12')
-                ->description('3% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success')
         ];
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -12,12 +12,5 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 38540; $i <= 961461; $i++) {
-            $user = new \App\Models\User();
-            $user->name = "User" . $i;
-            $user->email = "user" . $i . "@example.com";
-            $user->password = bcrypt('password');
-            $user->save();
-        }
     }
 }
